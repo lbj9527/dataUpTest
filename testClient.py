9423806e031ac8fc4679c47ab153d86d6b89be73
lbj9527 <1147431798@qq.com>
2022-12-17 23:10:36 +0800

    cswfewv

--- testClient.py
+++ testClient.py
@@ -5,5 +5,6 @@ import time
 while True:
     ismojuopen = {'status' : "open"}
     r = requests.post("https://lbj9527.pythonanywhere.com/register", data=ismojuopen)
+    #r = requests.post("http://127.0.0.1:5000//register", data=ismojuopen)
     print(r)
     time.sleep(0.5)
